135e67da626387079837168f6a34924922ed7572
Bruno <brunocmoraes97@gmail.com>
2022-03-09 16:29:41 -0300

    Implementa leitura de arquivos csv

--- src/components/InputFile.js
+++ src/components/InputFile.js
@@ -2,12 +2,62 @@ import React, { useContext, useState } from 'react';
 import AppContext from '../context/AppContext';
 
 function InputFile() {
-  const [file, setFile] = useContext(AppContext);
+  const { file, setFile, setHeader, header, setContent, content }  = useContext(AppContext);
+
+  const reader = new FileReader();
+
+  const handleFile = ({ target }) => {
+    setFile(target.files[0]);
+  };
+
+  const handleSubmitFile = () => {
+    if(file) {
+      reader.onload = function(event) {
+        const textCsv = event.target.result;
+        const [head, ...contents] = textCsv.split('\n');
+        setHeader(head.split(','));
+        const conteudo = contents.map((lines) => lines.split(','));
+        setContent(conteudo);
+      };
+      reader.readAsText(file);
+    };
+  };
 
   return (
     <div>
-      
-    </div>
+      <h1>Bem vindo!</h1>
+      <h2>Escolha um arquivo ou arraste</h2>
+      <input type="file" onChange={ handleFile } accept=".csv" />
+      <button type="button" onClick={ handleSubmitFile }>Enviar</button>
+      <table>
+        <thead>
+          <tr>
+            { header &&
+              Array.from({ length: 2 }).map((content, index) => (
+                header.map((headerName) => (
+                  <th>
+                    <input
+                      id={index}
+                      key={index}
+                      defaultValue={index === 0 ? headerName : '' }
+                    />
+                  </th>
+                ))
+              ))
+            }
+          </tr>
+        </thead>
+        <tbody>
+        { content &&
+            content.map((item, index) => (
+              <tr>
+              { item.map((arquive) => (<td><input placeholder={arquive}/></td>)) }
+              </tr>)
+            )
+        }
+        </tbody>
+      </table>
+     </div>
   );
 }
 
--- src/context/AppProvider.js
+++ src/context/AppProvider.js
@@ -3,11 +3,17 @@ import AppContext from './AppContext';
 
 function AppProvider({ children }) {
   const [file, setFile] = useState(null);
+  const [header, setHeader] = useState(null);
+  const [content, setContent] = useState(null);
 
   const contextValue = {
     file,
     setFile,
-  }
+    header,
+    setHeader,
+    content,
+    setContent
+  };
 
  return (
     <AppContext.Provider value={ contextValue }>
